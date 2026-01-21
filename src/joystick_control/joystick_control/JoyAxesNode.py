5b26d43ce234a82ef8ed94f2fc13726ea3a444fe
Dev Arora <f20240537@pilani.bits-pilani.ac.in>
2026-01-21 19:03:37 +0530

    Feature : Can move in X,Z with joystick now hopefully

--- src/joystick_control/joystick_control/JoyAxesNode.py
+++ src/joystick_control/joystick_control/JoyAxesNode.py
@@ -3,17 +3,27 @@ from rclpy.node import Node
 from sensor_msgs.msg import Joy
 from std_msgs.msg import Int32MultiArray
 from joymap import mapJoystickToAction, mapJoyAxes
+from poseToPWM import pose_to_encoder, encoder_to_pose
+
+X_Z_STEP = 50
 
 class JoyAxes(Node):
     def __init__(self):
         super().__init__('joynode')
         self.sub = self.create_subscription(Joy, '/joy', self.joycallback, 10)
+        self.encsub = self.create_subscription(Int32MultiArray, '/arm_encoder_data', self.encCallback, 10)
         self.publisher = self.create_publisher(Int32MultiArray, "arm_target_states", 10)
         self.pwmpub = self.create_publisher(Int32MultiArray, "arm_pwm_commands", 10)
 
+    def encCallback(self, msg):
+        global lowerEnc
+        global upperEnc
+        lowerEnc = msg.data[0]
+        upperEnc = msg.data[1]
+
     def joycallback(self, msg):
 
-        # self.buttons = msg.buttons
+        self.buttons = msg.buttons
         self.axes = msg.axes
 
         joyAx = []
@@ -25,9 +35,27 @@ class JoyAxes(Node):
                     joyAx.append(-1)
             else:
                 joyAx.append(0)
-        
 
-        pwmPubVal = mapJoyAxes(joyAx)
+        if(self.buttons[0] != 1):
+            pwmPubVal = mapJoyAxes(joyAx)
+        else:
+            initialx, initialz = encoder_to_pose(lower_encoder = lowerEnc, upper_encoder = upperEnc)
+            finalx, finalz = 0, 0
+            if(joyAx[1] == 1):
+                finalx = initialx + X_Z_STEP
+                finalz = initialz
+            elif(joyAx[1] == -1):
+                finalx = initialx - X_Z_STEP
+                finalz = initialz
+            elif(joyAx[0] == 1):
+                finalx = initialx
+                finalz = initialz + X_Z_STEP
+            elif(joyAx[0] == -1):
+                finalx = initialx
+                finalz = initialz - X_Z_STEP
+            
+            finalUpperEnc, finalLowerEnc = pose_to_encoder(finalx, finalz)
+            pwmPubVal = [finalUpperEnc, finalLowerEnc,0,0,0]
 
         pwmMsg = Int32MultiArray()
         pwmMsg.data = pwmPubVal
